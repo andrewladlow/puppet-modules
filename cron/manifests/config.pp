34b1f6d0ed4c27f735481819e2790affc8831dc0
Andrew Ladlow <andrew.ladlow@gmail.com>
2019-11-19 23:20:09 +0000

    update cron path

--- cron/manifests/config.pp
+++ cron/manifests/config.pp
@@ -1,7 +1,7 @@
 class cron::config {
 
   cron { 'puppet agent -t':
-    command => 'puppet agent -t',
+    command => '/opt/puppetlabs/bin/puppet agent -t',
     user    => 'root',
     minute  => fqdn_rand(59),
   }
