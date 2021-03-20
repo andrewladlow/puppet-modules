d000e9bba07485201e19fcfe172d85c13b1f1579
Andrew Ladlow <andrew.ladlow@gmail.com>
2021-03-20 22:10:12 +0000

    Add local bashrc depending on homedir

--- bash_prompt/manifests/config.pp
+++ bash_prompt/manifests/config.pp
@@ -4,6 +4,17 @@ class bash_prompt::config {
     content => file('bash_prompt/bashrc'),
   }
 
+  $homedir = $facts['identity']['user'] ? {
+    'root'  => '/root',
+  } 
+
+  file { "local .bashrc":
+    ensure  => file,
+    content => file('bash_prompt/bashrc'),
+    path    => "${homedir}/.bashrc",
+  }
+
+
   file { '/etc/screen.screenrc':
     ensure  => file,
     content => file('bash_prompt/screenrc'),
