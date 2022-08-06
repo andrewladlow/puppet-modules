86d0a91ecde6dcd3fe612fd6f05bd4a41041834c
Andrew Ladlow <andrew.ladlow@gmail.com>
2022-08-07 00:05:49 +0100

    Load Restic cron config from template

--- restic/manifests/config.pp
+++ restic/manifests/config.pp
@@ -2,12 +2,6 @@ class restic::config {
 
   $restic_repo_pass = lookup('restic_repo_pass')
 
-  $file="#!/bin/bash
-export RESTIC_PASSWORD=\"${restic_repo_pass}\"
-export GOGC=20
-restic -r sftp:rs3:/storage/restic-backups --verbose backup / --exclude-file=/root/restic-excludes.txt --tag $(hostname -s)
-"
-
   file { '/root/restic-excludes.txt':
     ensure  => file,
     content => file('restic/restic-excludes.txt'),
@@ -17,8 +11,7 @@ restic -r sftp:rs3:/storage/restic-backups --verbose backup / --exclude-file=/ro
     ensure  => file,
     mode    => '0755',
     replace => yes, 
-    #content => file("restic/cron_daily-restic"),
-    content => $file
+    content => template("restic/cron_daily-restic"),
   }
 
   exec { 'Update Restic':
