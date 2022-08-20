8e230e87acad66840a92838b4c321bade7d56653
Andrew Ladlow <andrew.ladlow@gmail.com>
2022-08-20 20:34:59 +0100

    Update restic config.pp formatting

--- restic/manifests/config.pp
+++ restic/manifests/config.pp
@@ -10,15 +10,15 @@ class restic::config {
   file { '/etc/cron.daily/restic':
     ensure  => file,
     mode    => '0755',
-    replace => yes, 
-    content => template("restic/cron_daily-restic"),
+    replace => yes,
+    content => template('restic/cron_daily-restic'),
   }
 
   exec { 'Update Restic':
     command => 'restic self-update',
     path    => '/usr/local/bin/:/usr/bin/',
     # Compare current version number with latest from repo to determine whether to update or not
-    onlyif  => "diff <(restic version | grep -oE '([0-9]{1,2}\.?)+' | head -1) <(curl -s https://api.github.com/repos/restic/restic/releases/latest | grep 'tag_name' | grep -oE '([0-9]{1,2}\.?)+')",
+    onlyif  => 'diff <(restic version | grep -oE "([0-9]{1,2}\.?)+" | head -1) <(curl -s https://api.github.com/repos/restic/restic/releases/latest | grep "tag_name" | grep -oE "([0-9]{1,2}\.?)+")',
   }
 }
 
