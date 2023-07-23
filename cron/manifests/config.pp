06c5ede7e8f1ddb3a62ab6c65fe54be84ced1eaf
Andrew Ladlow <andrew.ladlow@gmail.com>
2023-07-23 11:43:02 +0100

    Ensure Puppet cron is present

--- cron/manifests/config.pp
+++ cron/manifests/config.pp
@@ -1,6 +1,7 @@
 class cron::config {
 
   cron { 'puppet agent -t':
+    ensure  => 'present',
     command => '/opt/puppetlabs/bin/puppet agent -t',
     user    => 'root',
     minute  => fqdn_rand(59),
