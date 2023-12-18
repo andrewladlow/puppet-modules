fb8a5f3d5fb2e78cf0cb152a04d4218b1e24f382
Andrew Ladlow <andrew.ladlow@gmail.com>
2023-12-18 23:58:34 +0000

    Append dev/null redirect

--- cron_puppet/manifests/config.pp
+++ cron_puppet/manifests/config.pp
@@ -2,7 +2,7 @@ class cron_puppet::config {
 
   cron { 'puppet agent -t':
     ensure      => 'present',
-    command     => 'puppet agent -t',
+    command     => 'puppet agent -t > /dev/null',
     user        => 'root',
     minute      => fqdn_rand(59),
     environment => 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin:/opt/dell/srvadmin/bin:/opt/dell/srvadmin/sbin',
