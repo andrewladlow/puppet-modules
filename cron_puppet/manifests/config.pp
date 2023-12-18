9080b4ab88ffa0d70705b2c6c242abf490383eea
Andrew Ladlow <andrew.ladlow@gmail.com>
2023-12-18 02:08:27 +0000

    Call Puppet binary using env path rather than direct path

--- cron_puppet/manifests/config.pp
+++ cron_puppet/manifests/config.pp
@@ -2,7 +2,7 @@ class cron_puppet::config {
 
   cron { 'puppet agent -t':
     ensure  => 'present',
-    command => '/opt/puppetlabs/bin/puppet agent -t',
+    command => 'puppet agent -t',
     user    => 'root',
     minute  => fqdn_rand(59),
   }
